a1d2764483a5f901bc4f45d296e1db3dc3191e92
GQIRiza <128170764+GQIRiza@users.noreply.github.com>
2023-04-04 20:28:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import datetime
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
+        #задание параметров окна
         self.title("Planerovsschic")
         self.geometry("1100x700")
 
@@ -43,11 +43,11 @@ class App(customtkinter.CTk):
 
 
 
-                    # set grid layout 1x2
+        #параметры сетки
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        # load images with light and dark mode image
+        #подгрузка картинок
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
         self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
@@ -63,26 +63,26 @@ class App(customtkinter.CTk):
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
-
+        #параметры для лого
         self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Image Example", image=self.logo_image,
                                                              compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
-
+        #параметры для кнопки календарь
         self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Calendar",
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                    image=self.home_image, anchor="w", command=self.home_button_event)
         self.home_button.grid(row=1, column=0, sticky="ew")
-
+        # параметры для кнопки проекты
         self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Projects",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                       image=self.chat_image, anchor="w", command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
-
+        # параметры для кнопки конспекты
         self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Conspects",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                       image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
         self.frame_3_button.grid(row=3, column=0, sticky="ew")
-
+        # параметры для кнопки смены темы
         self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
                                                                 command=self.change_appearance_mode_event)
         self.appearance_mode_menu.grid(row=6, column=0, padx=100, pady=20, sticky="s")
@@ -96,7 +96,7 @@ class App(customtkinter.CTk):
         #self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="", image=self.large_test_image)
         #self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
 
-
+        # параметры для кнопки листания вперед назад на календаре
         self.back_button = customtkinter.CTkButton(self.home_frame,  corner_radius=0, height=50,width=50,
                                                      border_spacing=1, text="<", compound="left" )
         self.back_button.grid(row=0, column=0, padx=0, pady=0)
@@ -104,13 +104,14 @@ class App(customtkinter.CTk):
         self.next_button = customtkinter.CTkButton(self.home_frame, corner_radius=0, height=50, border_spacing=1,width=50,
                                                      text=">", compound="left")
         self.next_button.grid(row=0, column=6, padx=0, pady=0)
-
+        # параметры для Отображения месяца нв календаре
         self.info_label = customtkinter.CTkLabel(self.home_frame, text='012', width=1, height=1)
         self.info_label.grid(row=0, column=1, columnspan=5, sticky="NSEW")
-
+        #кнопки с месяцами
         for n in range (0,7):
             self.dni=customtkinter.CTkLabel(self.home_frame, text=calendar.day_abbr[n], width=50, height=50, fg_color='blue')
             self.dni.grid(row=1, column=n, sticky="NSEW", )
+        #кнопки с днями на календаре
         for row in range(5):
             for col in range(7):
                 self.dni= customtkinter.CTkButton(self.home_frame, text="0", width=100, height=100)
